42c0e68d62d60ce16a9fc883d65b8f62b37c4526
rnt-ash <l.koch@aronet.ch>
2017-03-23 16:42:56 +0100

    Add loading screen to confirm dialog

--- public/js/bootbox.js
+++ public/js/bootbox.js
@@ -11,11 +11,27 @@ $( document ).ready(function() {
                 },
                 confirm: {
                     label: '<i class="fa fa-check"></i> Yes',
+                    className: 'btn-primary loadingScreen'
                 },
             },
             callback: function (result) {
+                loadingScreen();
                 if(result) window.location = link;
             } 
         });
     });
 });
+
+function loadingScreen() {
+    $('body').append(
+        "<div class='loaderBackground'>"+
+            "<div class='spinnerWrapper'>"+
+                "<div class='spinner'>"+
+                    "<i class='fa fa-spinner fa-pulse fa-5x fa-fw'></i>"+
+                    "<div class='loaderText'>Loading ...</div><br /><br />"+
+                "</div>"+
+            "</div>"+
+        "</div>"
+    );
+    $('body').css("overflow","hidden");
+}
