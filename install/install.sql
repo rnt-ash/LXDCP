871aae36cc63308cabadac6ab872782d5632b8ea
rnt-maple <y.wechsler@aronet.ch>
2017-03-29 09:47:24 +0200

    Remove last_rrd_run from mon_local_jobs

--- install/install.sql
+++ install/install.sql
@@ -166,7 +166,6 @@ CREATE TABLE IF NOT EXISTS `mon_local_jobs` (
   `mon_contacts_message` text NOT NULL COMMENT 'FK logins, comma separated value',
   `mon_contacts_alarm` text NOT NULL COMMENT 'FK logins, comma separated value',
   `last_run` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `last_rrd_run` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
